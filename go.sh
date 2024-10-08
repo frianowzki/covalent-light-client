75bcf90a479d2830ebf1186cec85faf7b11a3f6c
frianowzki <54317384+frianowzki@users.noreply.github.com>
2024-10-09 02:48:23 +0700

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,12 @@
+wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+
+echo "" >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
+
+source ~/.bashrc
+
+go version
